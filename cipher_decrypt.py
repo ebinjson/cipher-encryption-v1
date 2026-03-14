4b4ca4d2321dd36fc0b84bb35f47b1eb0a6eac31
Ebin Jaison <ebincjaison2009@gmail.com>
2026-03-14 10:02:27 +0000

    Add files via upload

---
+++ cipher_decrypt.py
@@ -0,0 +1,19 @@
+#ord : - to convert string to ascii decimal
+#chr : - to convert decimal to string
+
+
+#text to decimal to text
+word = input("Enter the word to decrypt: ")
+length = len(word)
+Ascii_arr = []
+text_arr = []
+for i in word:
+    ascii_value = ord(i)
+    new_ascii_value = ascii_value - 2 #key
+    Ascii_arr.append(new_ascii_value)
+for i in Ascii_arr:
+    text = chr(i)
+    text_arr.append(text)
+
+msg = "".join(text_arr)
+print(f"Decrypted message: {msg}")
